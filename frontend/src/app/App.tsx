23e9bd37cedb5fc70474835cc19e16e796efc635
potatoyz908 <euller2005@gmail.com>
2024-12-20 13:48:03 -0300

    Feat(Front-end): Inicializado a estrutura do frontend com React, Redux e front básico para usar o CRUD

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reloadwd.
+        </p>
+      dntro  <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           oii
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/resources/static/scripts.js
@@ -0,0 +1,40 @@
+const apiUrl = "http://localhost:8080/filmes";
+
+document.getElementById("add-film-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const genre = document.getElementById("genre").value;
+    const releaseYear = document.getElementById("releaseYear").value;
+
+    const response = await fetch(${apiUrl}/add_filme, {
+    method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, genre, release_year }),
+});
+
+if (response.ok) {
+    alert("Filme adicionado com sucesso!");
+    loadFilms();
+} else {
+    alert("Erro ao adicionar o filme.");
+}
+})
+
+async function loadFilms() {
+    const response = await fetch(${apiUrl}/listar_filmes);
+    const films = await response.json();
+
+    const filmList = document.getElementById("films");
+    filmList.innerHTML = "";
+
+    films.forEach((film) => {
+        const li = document.createElement("li");
+        li.textContent = ${film.title} (${film.genre}, ${film.release_year});
+        filmList.appendChild(li);
+    });
+}
+
+window.onload = loadFilms;
